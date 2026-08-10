4026144ed3978a832e9340ecb439ed4732f3a5a1
Gabriel Turbino <gabrielzturbino@gmail.com>
2026-08-10 20:21:50 -0300

    Add files via upload

---
+++ exercicio3.c
@@ -0,0 +1,27 @@
+//exercicio 3 da apostila//
+
+#include <stdio.h> //scanf, printf
+
+int main(){
+        
+        char aluno[20]; //se não definir o valor precisa definir o tamanho maximo//
+        printf("Digite o nome do aluno: ");
+
+        scanf("%s", aluno);
+        
+        float nota1, nota2, nota3; //definição de variaveis do mesmo tipo pode estar na mesma linha//
+        printf("Digite a primeira nota: ");
+        scanf("%f", &nota1);
+        
+        printf("Digite a segunda nota: ");
+        scanf("%f", &nota2);
+        
+        printf("Digite a terceira nota: ");
+        scanf("%f", &nota3);
+        
+        float media = (nota1 + nota2 + nota3)/3;
+        printf("A media das notas de %s é: %f", aluno, media);
+        
+    return 0;
+        
+}
