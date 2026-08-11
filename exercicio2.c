513e8ed4c3a55cac4a83905a3b32165d81993774
Gabriel Turbino <gabrielzturbino@gmail.com>
2026-08-11 14:46:51 -0300

    Update exercicio2.c
    adicionado: comentários ao código.

--- exercicio2.c
+++ exercicio2.c
@@ -8,9 +8,9 @@ int main(){
         printf("Digite o valor em graus Celsius para converção: ");
         scanf("%f", &Celsius);
         
-        float F = Celsius * 9/5 + 32;
+        float F = Celsius * 9/5 + 32; //caso tivesse uma operação com prioridade que mudaria a ordem de execução esperada é necessário usar ()//
         
-        printf("A converção de %.2f graus Celsius para Fahrenheit é: %.2f", Celsius, F);
+        printf("A converção de %.2f graus Celsius para Fahrenheit é: %.2f", Celsius, F); //lembrar do ; no final de cada linha//
     
     return 0;
         
