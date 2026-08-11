2b8cf08079895df9d49e5abcdccd7250b6e03482
Gabriel Turbino <gabrielzturbino@gmail.com>
2026-08-11 14:49:57 -0300

    Update exercicio3.c
    comentários

--- exercicio3.c
+++ exercicio3.c
@@ -7,7 +7,7 @@ int main(){
         char aluno[20]; //se não definir o valor precisa definir o tamanho maximo//
         printf("Digite o nome do aluno: ");
 
-        scanf("%s", aluno);
+        scanf("%s", aluno); //como é string não precisa do &//
         
         float nota1, nota2, nota3; //definição de variaveis do mesmo tipo pode estar na mesma linha//
         printf("Digite a primeira nota: ");
