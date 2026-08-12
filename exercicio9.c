35968da54517af7ba83705793c0b5da3bfa7e6a5
Gabriel Turbino <gabrielzturbino@gmail.com>
2026-08-12 14:26:51 -0300

    Add files via upload

---
+++ exercicio9.c
@@ -0,0 +1,41 @@
+//exercicio 9//
+
+#include <stdio.h>
+
+int main()
+{
+        int valor;
+        printf("Digite valor em reais que deseja saber a menor quantidade de cedulas necessárias: ");
+        scanf("%d", &valor);
+        
+        int nota100 = valor / 100;
+        valor = valor % 100;
+    
+        int nota50 = valor / 50;
+        valor = valor % 50;
+    
+        int nota20 = valor / 20;
+        valor = valor % 20;
+    
+        int nota10 = valor / 10;
+        valor = valor % 10;
+    
+        int nota5 = valor / 5;
+        valor = valor % 5;
+    
+        int nota2 = valor / 2;
+        valor = valor % 2;
+    
+        int nota1 = valor / 1;
+        valor = valor % 1;
+    
+        printf("%d nota(s) de 100,00\n", nota100);
+        printf("%d nota(s) de 50,00\n", nota50);
+        printf("%d nota(s) de 20,00\n", nota20);
+        printf("%d nota(s) de 10,00\n", nota10);
+        printf("%d nota(s) de 5,00\n", nota5);
+        printf("%d nota(s) de 2,00\n", nota2);
+        printf("%d nota(s) de 1,00\n", nota1);
+
+    return 0;
+}
