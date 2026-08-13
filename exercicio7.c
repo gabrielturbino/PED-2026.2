0a4c402b84a8bc876a32911c92fe4d63a6a20d7b
Gabriel Turbino <gabrielzturbino@gmail.com>
2026-08-12 21:43:26 -0300

    Update exercicio7.c
    variáveis pós main.

--- exercicio7.c
+++ exercicio7.c
@@ -4,15 +4,13 @@
 
 int main()
 {
-        int n1;
+        int n1, n2, temp; //com o temp estou usando uma variavel para armazenar temporariamente o valor de um dos números//
         printf("Digite o primeiro número: ");
         scanf("%d", &n1);
         
-        int n2;
         printf("Digite o segundo número: ");
         scanf("%d", &n2);
-        
-        int temp; //usando uma variavel para armazenar temporariamente o valor de um dos números//
+
         temp = n1;
         n1=n2;
         n2=temp;
