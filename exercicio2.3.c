3b9a94d83f54abb4e03884af8956d26d490e9c1e
Gabriel Turbino <gabrielzturbino@gmail.com>
2026-08-12 22:31:05 -0300

    Add files via upload

---
+++ exercicio2.3.c
@@ -0,0 +1,29 @@
+//exercicio 2.3//
+
+#include <stdio.h>
+
+int main()
+{
+        int n1,n2,n3;
+        
+        printf("Digite o primeiro número: ");
+        scanf("%d", &n1);
+        
+        printf("Digite o segundo número: ");
+        scanf("%d", &n2);
+        
+        printf("Digite o terceiro número: ");
+        scanf("%d", &n3);
+        
+        if (num n1 > n2 and n1 > n3) {
+            printf("\nO número %d é o maior.",n1); 
+        }
+        else if (n2 > n1 and n2 > n3) {
+            printf("\nO número %d é o maior.", n2); 
+        }
+        else {
+            printf("\nO número %d é o maior.", n3);
+        }
+    
+   return 0;
+}
