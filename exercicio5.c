78f9caed00b69bd8c7d1001df604e012ffa39b37
Gabriel Turbino <gabrielzturbino@gmail.com>
2026-08-12 21:36:40 -0300

    Update exercicio5.c
    variáveis após main

--- exercicio5.c
+++ exercicio5.c
@@ -4,17 +4,16 @@
 
 int main()
 {
-        int num;
+        int num, hora, minutos, segundos;
+        
         printf("Digite o número em segundos que deseja converter: ");
         scanf("%d", &num);
         
-        int hora = num/3600; //por ser int vai ignorar o resto//
-        int minutos = (num % 3600)/60; //vai usar somente o resto do calculo da hora para os minutos//
-        int segundos = num % 60;
+        hora = num/3600; //por ser int vai ignorar o resto//
+        minutos = (num % 3600)/60; //vai usar somente o resto do calculo da hora para os minutos//
+        segundos = num % 60;
         
         printf("%d convertidos vira: %d horas, %d minutos e %d segundos.", num,hora,minutos,segundos);
-        
-    
 
     return 0;
 }
