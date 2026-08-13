b15892484ab000c250afe9ec28930d303cedeacb
Gabriel Turbino <gabrielzturbino@gmail.com>
2026-08-12 21:46:15 -0300

    Update exercicio8.c
    variáveis pós main.

--- exercicio8.c
+++ exercicio8.c
@@ -4,11 +4,10 @@
 
 int main()
 {
-        int n1;
+        int n1, n2;
         printf("Digite o primeiro número: ");
         scanf("%d", &n1);
         
-        int n2;
         printf("Digite o segundo número: ");
         scanf("%d", &n2);
         
