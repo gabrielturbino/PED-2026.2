f6b9dcedf1ccc2c2364a4e201648e9d9f4de4d9a
Gabriel Turbino <gabrielzturbino@gmail.com>
2026-08-12 21:33:30 -0300

    Update exercicio4.c
    atualização com comentários e com declaração de variáveis após o main.

--- exercicio4.c
+++ exercicio4.c
@@ -3,13 +3,15 @@
 #include <stdio.h>
 #include <math.h>
 
+//poderia declarar aqui a contante caso a biblioteca apresentasse erro, com: #define//
 int main() {
     
-        float raio;
+        float raio, area;
+    
         printf("Digite o raio do círculo que deseja saber a área: ");
         scanf("%f", &raio);
         
-        float area = M_PI * (raio*raio); //pode usar M_PI * pow(raio, 2) ou seja potencia do raio elevado a 2//
+        area = M_PI * (raio*raio); //pode usar M_PI * pow(raio, 2) ou seja potencia do raio elevado a 2//
         printf("Um circulo com %.2f de raio tem %.2f de área",raio, area);
         
     
